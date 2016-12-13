9a39f210a7b67520b25e93340b29169ab56f1965
Michael Parque <mmp23456@gmail.com>
2016-12-12 20:22:45 -0500

    Added Vector data structure.
    The Vector data structure will be used to perform geometric
    calculations that will lead us to our solution. The solution basically
    being an addition of vectors from the starting point where the
    vector to use in the next step of the algorithm is a function of
    current facing direction (start North) and the Turn argument (from
    input).

--- day1/solution.py
+++ day1/solution.py
@@ -5,6 +5,12 @@ FILENAME = "input.txt"
 
 
 class Turn:
+	"""Turn represents an input of our algorithm.
+
+	Instance variables:
+	direction -- direction of the turn relative to facing direction
+	steps -- number of steps taken after the turn
+	"""
 	def __init__(self, rawTurn):
 		self.direction = rawTurn[0]
 		self.steps = int(rawTurn[1:])
@@ -17,8 +23,26 @@ class Turn:
 		return str(self)	
 
 
+class Vector:
+	"""Vector used in geometric calculations to determine coordinates.
+
+	VectorA + VectorB = VectorC where VectorC has start point of VectorA
+	and end point of VectorB if VectorA endpoint = VectorB startpoint
+	
+	Instance variables:
+	startPoint -- the starting point of the vector
+	endPoint -- the end point of the vector
+	"""
+
+	def __init__(self, startPoint, endPoint):
+		self.startPoint = startPoint
+		self.endPoint = endPoint
+
+
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
 		rawInput = inputFile.readline()
 		inputTurns = map(lambda raw: Turn(raw.strip()), rawInput.split(","))
-		print(list(inputTurns))
+
+	for turn in inputTurns:
+		print(turn)
