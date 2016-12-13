8679290269e1b2d153070ba7b31a40e2553e0a39
Michael Parque <mmp23456@gmail.com>
2016-12-13 15:33:05 -0500

    Solve part one of day 3 puzzle.
    Read in all lines as triangles and check the triangle constraint.

--- day3-squares-with-three-sides/squares-with-three-sides.py
+++ day3-squares-with-three-sides/squares-with-three-sides.py
@@ -4,14 +4,36 @@
 FILENAME = "input.txt"
 
 
+def isPossibleTriangle(triangle):
+	"""Check if a triangle is possible given the three sides.
+
+	Algorithm: sum up all sides and subtract each side from that sum.
+	If any of those operations results in a number <= side, reject.
+	"""
+	cumsum = sum(triangle)
+	for side in triangle:
+		if cumsum - side <= side:
+			return False
+	return True
+
+
 def checkPossibleTriangles(triangles):
+	possible = 0
 	for triangle in triangles:
-		print(triangle)
+		print("Triangle: {}".format(triangle))
+		if isPossibleTriangle(triangle):
+			possible += 1
+		else:
+			print("NOT POSSIBLE")
+		print("---")
+	return possible
 
 
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
-		checkPossibleTriangles(map(lambda line: tuple(int(side)
-												 	  for side 
-												 	  in line.strip().split()),
-								   inputFile.readlines()))
+		possible = checkPossibleTriangles(map(lambda line: 
+												tuple(int(side)
+												for side 
+												in line.strip().split()),
+								   		  inputFile.readlines()))
+		print("There are {} possible triangles.".format(possible))
