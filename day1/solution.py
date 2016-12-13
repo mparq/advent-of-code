e04d10726f582c44e462f22e45fcbe173d92b4e7
Michael Parque <mmp23456@gmail.com>
2016-12-13 00:13:18 -0500

    Fleshing out necessary data structures and added .gitignore file.
    - Added .gitignore.
    - Added Point data structure for more coordinate plane maths.
    - Updated Vector data structure with concatenate method which will pave
    the way for coordinate plane maths for solving the puzzle.

--- day1/solution.py
+++ day1/solution.py
@@ -11,6 +11,7 @@ class Turn:
 	direction -- direction of the turn relative to facing direction
 	steps -- number of steps taken after the turn
 	"""
+
 	def __init__(self, rawTurn):
 		self.direction = rawTurn[0]
 		self.steps = int(rawTurn[1:])
@@ -21,6 +22,29 @@ class Turn:
 
 	def __repr__(self):
 		return str(self)	
+
+
+class Point:
+	"""Data structure representing a point in the 2-d coordinate plane.
+
+	Instance variables:
+	x -- x coordinate
+	y -- y coordinate
+	"""
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def __eq__(self, pointB):
+		if not pointB or not isinstance(pointB, Point):
+			return False
+		return self.x == pointB.x and self.y == pointB.y
+
+	def __str__(self):
+		return "Point: x={0}, y={1}".format(self.x, self.y)
+
+	def __repr__(self):
+		return str(self)
 
 
 class Vector:
@@ -34,9 +58,40 @@ class Vector:
 	endPoint -- the end point of the vector
 	"""
 
-	def __init__(self, startPoint, endPoint):
+	def __init__(self, startPoint, xdelta=0, ydelta=0):
 		self.startPoint = startPoint
-		self.endPoint = endPoint
+		self.xcomponent = xdelta
+		self.ycomponent = ydelta
+		self.endPoint = Point(self.startPoint.x + xdelta, 
+							  self.startPoint.y + ydelta)
+		
+	def concatenate(self, vectorB):
+		"""Return the vector produced by appending given vector to this one.
+
+		Might not be the correct name for this method. Can only add a vector
+		which has the same start point as the end point of this vector. Will
+		be used to build up the final vector.
+		"""
+		if not vectorB or not isinstance(vectorB, Vector):
+			raise ValueError("argument given is not an instance of Vector.")
+
+		if vectorB.startPoint != self.endPoint:
+			raise ValueError("argument vector start point must match "
+							 "this vector's end point to concatenate.")
+
+		return Vector(
+			self.startPoint, 
+			self.xcomponent + vectorB.xcomponent,
+			self.ycomponent + vectorB.ycomponent)
+
+	def __str__(self):
+		return "Vector: ({start.x}, {start.y}) {x:+}x {y:+}y".format(
+			start=self.startPoint,
+			x=self.xcomponent,
+			y=self.ycomponent)
+
+	def __repr__(self):
+		return str(self)
 
 
 if __name__ == "__main__":
