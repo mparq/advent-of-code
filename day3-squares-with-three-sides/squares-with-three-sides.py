462cd38b283401ecbd68958c5c93ff621a7d1886
Michael Parque <mmp23456@gmail.com>
2016-12-13 16:04:02 -0500

    Added solution for part two of day 3.
    Just flipped the input on its side and partitioned by 3.

--- day3-squares-with-three-sides/squares-with-three-sides.py
+++ day3-squares-with-three-sides/squares-with-three-sides.py
@@ -19,21 +19,47 @@ def isPossibleTriangle(triangle):
 
 def checkPossibleTriangles(triangles):
 	possible = 0
-	for triangle in triangles:
+	for i, triangle in enumerate(triangles):
 		print("Triangle: {}".format(triangle))
 		if isPossibleTriangle(triangle):
 			possible += 1
 		else:
 			print("NOT POSSIBLE")
 		print("---")
+	print("Out of {} total.".format(i))
 	return possible
+
+
+def convertToTriangles(rawInput):
+	"""Convert raw input into triangles for parsing by algorithm.
+
+	Basically, we are reading in each line, keeping up a small data
+	structure which keeps track of the running triangles in each
+	column. After three lines have been read, we yield each of the
+	triangles which we have built for each column and proceed to
+	null out the data structure. In this way, we avoid double-reading
+	lines with some small book-keeping overhead.
+	"""
+	triangleSlice = None
+	for i, line in enumerate(rawInput):
+
+		row = tuple(int(side) for side in line.strip().split())
+
+		if i != 0 and i % 3 == 0:
+			for triangle in triangleSlice:
+				yield tuple(triangle)
+
+		if i % 3 == 0:
+			triangleSlice = [[] for column in row]
+			
+		for i, side in enumerate(row):
+			triangleSlice[i].append(side)
+
+	for triangle in triangleSlice:
+		yield tuple(triangle)
 
 
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
-		possible = checkPossibleTriangles(map(lambda line: 
-												tuple(int(side)
-												for side 
-												in line.strip().split()),
-								   		  inputFile.readlines()))
+		possible = checkPossibleTriangles(convertToTriangles(inputFile.readlines()))
 		print("There are {} possible triangles.".format(possible))
