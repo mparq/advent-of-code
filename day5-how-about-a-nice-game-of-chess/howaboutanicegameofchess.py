218e47cd828c7daee9d7774db6b43be325606f8a
Michael Parque <mmp23456@gmail.com>
2016-12-14 16:10:13 -0500

    Solved part 2 of day 5.
    Intense brute forcing...

--- day5-how-about-a-nice-game-of-chess/howaboutanicegameofchess.py
+++ day5-how-about-a-nice-game-of-chess/howaboutanicegameofchess.py
@@ -5,6 +5,8 @@ This puzzle seems to involve brute-forcing the correct code by
 using an increasing index appended to our door id. The characters at the
 6th index of the first 8 hashes that start with five zeroes becomes our
 password and the solution to the first part of this puzzle.
+
+Second part of the puzzle. More brute forcing. But more intense...
 """
 import hashlib
 
@@ -13,13 +15,18 @@ DOOR_ID = "reyedfim"
 
 
 if __name__ == "__main__":
-	password = []
+	password = [None for i in range(8)]
 	index = 0
-	while len(password) < 8:
+	while None in password:
 		tempBase = DOOR_ID + str(index)
 		tempHash = hashlib.md5(tempBase.encode("utf-8")).hexdigest()
 		if tempHash.startswith("00000"):
-			password.append(tempHash[5])
-			print(password)
+			print("Interesting hash: {}".format(tempHash))
+			position = int(tempHash[5]) if tempHash[5].isdigit() else None
+
+			if position is not None and 0 <= position < len(password) and password[position] is None:
+				password[position] = tempHash[6]
+			
+			print(" ".join("_" if letter is None else letter for letter in password))
 		index += 1
 	print("".join(password))
