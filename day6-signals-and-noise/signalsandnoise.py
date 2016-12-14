23d1f94226be287f1062477fdedcc0f1ef9aaac5
Michael Parque <mmp23456@gmail.com>
2016-12-14 17:11:14 -0500

    Completed part 2 of day 6
    - Added text to docstring explaining algorithm
    - Added leastFrequent property method to FrequencyCollector

--- day6-signals-and-noise/signalsandnoise.py
+++ day6-signals-and-noise/signalsandnoise.py
@@ -8,6 +8,17 @@ part one of this puzzle looks ripe for one, since we'll only be caring
 about the "best" letter in each column to construct our final output.
 I'll use a dictionary and min-heap (with negative counts) for each
 column to keep track of the most-frequent letter.
+
+Part two of the puzzle seems to just involve changing our pq from a
+max-heap to a min-heap (basically just flip the counts lol). It
+seems to be like that but... it won't be that easy. The priority
+queue implementation will have to be changed so that we truly modify
+the place of a letter in the queue each time one is added rather
+than just adding a new (count, letter) tuple to the queue. This
+shouldn't be too difficult actually. Rather than using the priority
+queue on-line implementation we did for part one, we just need to
+sort the dictionary at the end (this might actually turn out to be
+faster...)
 """
 import heapq
 from collections import defaultdict
@@ -24,6 +35,7 @@ class FrequencyCollector(object):
 
 	Instance variables:
 	mostFrequent -- The most frequent letter.
+	leastFrequent -- The least frequent letter.
 
 	Instance methods:
 	addLetter -- Adding a letter and updating inner data structures.
@@ -35,11 +47,16 @@ class FrequencyCollector(object):
 
 	def addLetter(self, letter):
 		self.counter[letter] += 1
-		heapq.heappush(self.pq, (-self.counter[letter], letter))
+		heapq.heappush(self.pq, (self.counter[letter], letter))
 
 	@property
 	def mostFrequent(self):
 		return self.pq[0][1]
+
+	@property
+	def leastFrequent(self):
+		return sorted((count, letter)
+					  for letter, count in self.counter.items())[0][1]
 
 
 def decodeFrequencySignals(signals):
@@ -51,8 +68,10 @@ def decodeFrequencySignals(signals):
 		for freq, letter in zip(columnFrequencies, signal):
 			freq.addLetter(letter)
 			print("Added letter: {}".format(letter))
-			print("Top 5 {}".format(freq.pq[:5]))
-	return "".join(freq.mostFrequent for freq in columnFrequencies)
+			print("Bottom five: {}"
+				.format(sorted((count, letter)
+							   for letter, count in freq.counter.items())[:5]))
+	return "".join(freq.leastFrequent for freq in columnFrequencies)
 
 
 if __name__ == "__main__":
