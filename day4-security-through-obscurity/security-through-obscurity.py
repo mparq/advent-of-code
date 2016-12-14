5e6709ba7d52dcd0cbfe5148519fa06ab393e3ba
Michael Parque <mmp23456@gmail.com>
2016-12-14 15:35:44 -0500

    Solved day4
    - Added code to verify parsed input.
    - Added code to sum up sector ids for part one.
    - Added code to print out shifted ciphers for part two. The output
    needed to be grepped for North Pole to find the solution.

--- day4-security-through-obscurity/security-through-obscurity.py
+++ day4-security-through-obscurity/security-through-obscurity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+import heapq
+from string import ascii_lowercase
 
 
 FILENAME = "input.txt"
+LETTER_TO_INDEX = {letter: index for index, letter in enumerate(ascii_lowercase)}
 
 
 def parseInput(line):
@@ -13,7 +16,47 @@ def parseInput(line):
 	return encrypted, sectorId, checkSum
 
 
+def checkValid(encrypted, checkSum):
+	counter = {}
+	ordered = []
+	for letter in encrypted:
+		if letter not in counter:
+			counter[letter] = 0
+		counter[letter] += 1
+	ordered = sorted((-count, letter) for letter, count in counter.items())
+	realSum = "".join(map(lambda tup: tup[1], ordered[:5]))
+	return realSum == checkSum
+	
+
+def sumSectorIds(lines):
+	total = 0
+	for line in lines:
+		print(line.strip())
+		encrypted, sectorId, checkSum = parseInput(line)
+		if checkValid(encrypted, checkSum):
+			print("Check sum was valid")
+			total += sectorId
+	return total
+
+
+def shiftCipher(encrypted, shift):
+	newString = []
+	for letter in encrypted:
+		newIndex = (LETTER_TO_INDEX[letter] + shift) % 26
+		newLetter = ascii_lowercase[newIndex]
+		newString.append(newLetter)
+	return "".join(newString)
+
+
+def solveCiphers(lines):
+	for line in lines:
+		encrypted, sectorId, checkSum = parseInput(line)
+		if checkValid(encrypted, checkSum):
+			solved = shiftCipher(encrypted, sectorId)
+			print("{} sectorID: {}".format(solved, sectorId))
+
+
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
-		for line in inputFile:
-			print(parseInput(line))
+		# grep output for northpole to solve
+		solveCiphers(inputFile.readlines())
