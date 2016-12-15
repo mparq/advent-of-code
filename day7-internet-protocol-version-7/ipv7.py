ee21e88a8e6ce80d33c8ad3bc1bff4549f8f7fe2
Michael Parque <mmp23456@gmail.com>
2016-12-15 10:15:56 -0500

    day7 - solve part one
    - Used my own findAbba algorithm which just searches through the string
    for same characters and then around those characters for different same
    characters.
    - splitIpAddress function added to split input ip addresses into
    valid ip sequences and invalid hypernet sequences through which to
    check for abbas.

--- day7-internet-protocol-version-7/ipv7.py
+++ day7-internet-protocol-version-7/ipv7.py
@@ -14,7 +14,7 @@ findabba(s)
 	while i < len(s) - 2
 		c = s[i]
 		if c == s[i+1]
-			if s[i-1] == s[i+2]
+			if c != s[i-1] and s[i-1] == s[i+2]
 				abba
 we have a very short pattern, worst case O(4n)
 """
@@ -24,11 +24,72 @@ import re
 FILENAME = "input.txt"
 
 
-def findTLSSupported(ipAddresses):
-	for ipAddress in ipAddresses:
-		print(ipAddress)
+def findAbba(haystack):
+	index = 1
+	while index < len(haystack) - 2:
+		needle = haystack[index]
+		if needle == haystack[index + 1]:
+			if (needle != haystack[index - 1]
+			   		and haystack[index - 1] == haystack[index + 2]):
+				return haystack[index - 1 : index + 3]
+		index += 1
 
+
+def splitIpAddress(ipAddress):
+	split = {"valid": [], "invalid": []}
+	valid = True
+	current = []
+	for char in ipAddress:
+		if valid and char == "[":
+			split["valid"].append("".join(current))
+			current = []
+			valid = False
+		elif not valid and char == "]":
+			split["invalid"].append("".join(current))
+			current = []
+			valid = True
+		else:
+			current.append(char)
+	if len(current) > 0:
+		split["valid"].append("".join(current))
+	return split
+
+
+def findTLSSupported(ipAddresses):
+	"""Parse and locate abbas in valid/invalid sections
+
+	abba in invalid section means ip address is not supported.
+	Otherwise, the ip address supports TLS if an abba is found in one
+	of its valid sections.
+	"""
+	counter = 0
+	for ipAddress in ipAddresses:
+		split = splitIpAddress(ipAddress)
+		print("Split for {} is {}".format(ipAddress, split))
+		valid = True
+		for hypernet in split["invalid"]:
+			abba = findAbba(hypernet)
+			if abba is not None:
+				print("Abba found in invalid section {}: {}".format(hypernet, abba))
+				valid = False
+				break
+		if not valid:
+			print("Abba found in invalid section {}. {} does not support TLS".format(hypernet, ipAddress))
+			continue
+
+		valid = False
+		for ip in split["valid"]:
+			abba = findAbba(ip)
+			if abba is not None:
+				print("Abba found in valid section {}: {}".format(ip, abba))
+				valid = True
+				break
+		if valid:
+			print("IP address {} supports TLS".format(ipAddress))
+			counter += 1
+	return counter
 
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
-		findTLSSupported(line.strip() for line in inputFile)
+		print("{} IP addresses support TLS.".format(
+			findTLSSupported(line.strip() for line in inputFile)))
