0816f2567c9b9b8ef89afaf8506bb24c0248e6f5
Michael Parque <mmp23456@gmail.com>
2016-12-15 12:15:10 -0500

    day8 - add stub for TinyLED class
    This data structure will encapsulate the logic of our puzzle solution.
    Inner representation is a 2d list and the object will expose public
    methods matching the puzzle spec for interacting with the led screen.

--- day8-two-factor-authentication/twofactorauthentication.py
+++ day8-two-factor-authentication/twofactorauthentication.py
@@ -4,6 +4,27 @@
 FILENAME = "input.txt"
 
 
+class TinyLED:
+	"""Data structure representing the led screen specified in puzzle.
+
+	Instance variables:
+	board -- 2d list representing the led pixel screen
+	width -- width of screen
+	height -- height of screen
+	numPixelsOn -- number of pixels that are ON in the screen
+
+	Instance methods:
+	rect -- turns on all pixels in a rectangle at top-left of screen
+	rotateRow -- shifts pixels right in a row, rotating at boundary
+	rotateColumn -- shifts pixels down in a column, rotating at boundary
+	"""
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.board = [[False for j in range(width)] for i in range(height)]
+
+
 if __name__ == "__main__":
 	with open(FILENAME) as inputFile:
 		for line in inputFile:
