f358d4c38f7d6f22bd8af376e44b7fa852f085fa
Michael Parque <mmp23456@gmail.com>
2016-12-17 01:39:16 -0500

    day9 - solve part two
    - Ugly code
    - Modified parse_marker to return number of characters read to get marker
    - Created recursive find_length function to find length of each decompressed section after decompressing its output
    - More work than it was worth to keep the one-pass buffer strategy... but hey, it's memory efficient.

--- day9-explosives-in-cyberspace/explosivesincyberspace.py
+++ day9-explosives-in-cyberspace/explosivesincyberspace.py
@@ -5,10 +5,151 @@ Part One
 --
 Input should be tokenized to create a large list that ends up being joined together to form the
 output string. Input is given as one long string.
+
+Part Two
+--
+Should have known. We need to be smarter about parsing just for the length, rather
+than building up another string. Seems suited for a recursive solution. I tried to
+avoid reading the entire input into memory in the solution for part one, but it seems
+like that's the way to go here. We could save in memory costs by creating a temporary
+string every time we need to read characters in from a marker.
+
+len(section)
+
+length = 0
+for char in section:
+    if char != "(":
+        length += 1
+    else:
+        length += parse_marker(section) 
+
+for section in section:
+    return len(section)
+
+def parse_marker(input_buffer, max_len):
+    length = 1
+    marker_text = ""
+    while length < max_len:
+        c = input_buffer.read(1)
+        length += 1
+        if c == ")":
+            return (length,) + tuple(int(num) for num in marker_text.split("x"))
+        marker_text += c
+    return length, 0, 0
+
+def find_length(input_buffer, to_read = None):
+    chars_read = 0
+    len = 0
+    while chars_read < to_read:
+        char = input_buffer.read(1)
+        if char == "(":
+            after_parse, num_to_read, repeat = parse_marker(input_buffer, to_read - chars_read)
+            chars_read += after_parse
+            length += find_length(input_buffer, )
+        chars_read += 1
+
+
+(6x2)(1x2)A
+AAAA
+
+(6x2)(2x2)AB
+(2x2)A(2x2)AB
+A(AABAB
+
+(6x3)(2x2)AB
+(2x2)A(2x2)A(2x2)AB
+A(AA(AABAB
+
+(6x3)(2x3)AB
+(2x3)A(2x3)A(2x3)AB
+A(A(A(2x3)A(2x3)AB
+A(A(AA(A(A(2x3)AB
+A(A(AA(A(AABABAB
+
+Edge case: (8x2)(5x2)ABC...
+
+Decompressing (8x2) yields (5x2)ABC(5x2)ABC
+Decompressing that yields ABC(5ABC(5x2)ABC
+Decompressing that yields ABC(5ABCABC..ABC..
+
+So the (5x2) marker, being encapsulated in the (8x2) marker section was expanded twice,
+but we lost two characters due to overlap
+
+repeat*len(section, num_chars)
+
+What about (11x2)(6x3)(3x3)ABC?
+(6x3)(3x3)A(6x3)(3x3)ABC
+(3x3)A(3x3)A(3x3)A(6x3)(3x3)ABC
+A(3A(3A(3x3)A(3x3)A(6x3)(3x3)ABC
+A(3A(3AA(3A(3A(3x3)A(6x3)(3x3)ABC
+A(3A(3AA(3A(3AA(6A(6A(6x3)(3x3)ABC
+A(3A(3AA(3A(3AA(6A(6A(3x3)A(3x3)A(3x3)ABC
+A(3A(3AA(3A(3AA(6A(6AA(3A(3A(3x3)A(3x3)ABC
+A(3A(3AA(3A(3AA(6A(6AA(3A(3AA(3A(3A(3x3)ABC
+A(3A(3AA(3A(3AA(6A(6AA(3A(3AA(3A(3AABCABCABC
+
+well... anyway, it doesn't look like we need to deal with this edge case.
 """
 
 
 FILENAME = "input.txt"
+
+
+class PrematureParseEndException(Exception):
+    """Raise this if we've reached the end of a section while parsing a paren.
+
+    Luckily, there doesn't seem to be a case of this in the input given or else headache...
+    """
+    pass
+
+
+def find_length(input_buffer, chars_to_read=None):
+    """Find length of the next X characters in the input after decompressing.abs
+
+    Called recursively for each marker found, passing each length up to caller to compose the
+    final length of the decompressed string. Complicated and messy, but does the job for this
+    case.
+    """
+    if chars_to_read is None:
+        chars_to_read = float("inf")
+
+    chars_read = 0
+    length = 0
+    char = None
+    while chars_read < chars_to_read:
+        char = input_buffer.read(1)
+
+        if char.isspace():
+            continue
+
+        if char == "":
+            break
+
+        chars_read += 1
+        print("read char: {} {} of {}".format(char, chars_read, chars_to_read))
+
+        if char != "(":
+            length += 1
+        else:
+            inner_chars_read, marker_chars, repeat = parse_marker(input_buffer)
+            print("({}x{}) with {} chars to read, {} inner chars read, {} chars read".format(
+                marker_chars, repeat, chars_to_read, inner_chars_read, chars_read
+            ))
+            chars_read += inner_chars_read
+            if chars_read + marker_chars > chars_to_read:
+                raise PrematureParseEndException()
+            try:
+                inner_length = find_length(input_buffer, marker_chars) * repeat
+                print("inner_length of ({}x{}) was {}".format(marker_chars, repeat, inner_length))
+            except PrematureParseEndException:
+                print("({}x{}) with {} chars to read".format(
+                    marker_chars, repeat, chars_to_read - chars_read))
+                raise PrematureParseEndException()
+            chars_read += marker_chars
+            length += inner_length
+        print("total length: {}".format(length))
+
+    return length
 
 
 def parse_marker(input_buffer):
@@ -18,11 +159,14 @@ def parse_marker(input_buffer):
     read until the closing paren and then parse AxB string.
     """
     marker_text = ""
-    char = input_buffer.read(1)
+    char = ""
+    length = 0
     while char != ")":
         marker_text += char
         char = input_buffer.read(1)
-    return tuple(int(num) for num in marker_text.split("x"))
+        print("marker char: {}".format(char))
+        length += 1
+    return (length,) + tuple(int(num) for num in marker_text.split("x"))
 
 
 def read_and_repeat(input_buffer, num_chars, repeat):
@@ -68,6 +212,4 @@ if __name__ == "__main__":
     with open(FILENAME) as input_file:
         print("Length of compressed file (with markers): {}".format(len(input_file.read().strip())))
     with open(FILENAME) as input_file:
-        decompressed_string = decompress(input_file)
-        print(decompressed_string[:200])
-        print("The length of the decompressed string is {}".format(len(decompressed_string)))
+        print("The length of the decompressed string is {}".format(find_length(input_file)))
