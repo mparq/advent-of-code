15d338c6eab30cf711c2e946a62d2d299d20daa9
Michael Parque <mmp23456@gmail.com>
2016-12-22 02:32:27 -0500

    day22 - Created Graph structure and added sensible __str__ function.

--- day22-grid-computing/gridcomputing.py
+++ day22-grid-computing/gridcomputing.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+'''adventofcode.com/2016/day/22
+
+Part One
+--
+All we had to do was read in node inputs and sort two lists by used and avail.
+From this we can deduce that there is a viable pair for each node in the used
+list paired with every higher node from the avail list. This can be determined
+with one pass through each list.
+
+Part Two
+--
+Looks like we'll need to do another state-space searching algorithm.
+
+I want to try implementing IDA* (Iterative deepening A*).
+Simple heuristic function might be the number of steps it would take
+to send the current goal data to start along with the number of moves
+it would take to off-load the start's data.
+'''
 import re
 
 
@@ -6,20 +24,53 @@ FILENAME = 'input.txt'
 
 
 class Graph(object):
+    '''Graph object stores state of all nodes and represents a state in our state search.'''
 
-    def __init__(self, width, height, nodes):
+    def __init__(self, nodes, start=None, goal=None, width=37, height=25):
         self.width = width
         self.height = height
+        self.start = start
+        self.goal = goal
         self.graph = {}
         for node in nodes:
             self.graph[(node.x, node.y)] = node
-        print(self.graph)
+
+    def __str__(self):
+        row_strings = []
+        cutoff = 5
+        header_string = ('    ' + '    '.join('{:^9d}'.format(col) for col in range(cutoff))
+                         + ' ' * 17
+                         + '    '.join('{:^9d}'.format(col)
+                                       for col in range(self.width - (cutoff + 1), self.width)))
+        for row in range(self.height):
+            col_strings = []
+            for col in range(cutoff):
+                node = self.graph[(col, row)]
+                format_string = ('({:3d}/{:3d})' if node == self.start
+                                 else '[{:3d}/{:3d}]' if node == self.goal
+                                 else ' {:3d}/{:3d} ')
+                col_strings.append(format_string.format(node.used, node.size))
+            col_strings.append('    ..   ')
+            for col in range(self.width - (cutoff + 1), self.width):
+                node = self.graph[(col, row)]
+                format_string = ('({:3d}/{:3d})' if node == self.start
+                                 else ('[{:3d}/{:3d}]' if node == self.goal
+                                       else ' {:3d}/{:3d} '))
+                col_strings.append(format_string.format(node.used, node.size))
+            row_strings.append('{:<4d}{}'.format(row, ' -- '.join(col_strings)))
+        row_separator = '\n{}\n'.format('    '
+                                        + '    '.join('    |    ' for col in range(cutoff * 2)))
+        table_body = row_separator.join(row_strings)
+        return header_string + '\n\n' + table_body
 
 
 class Node(object):
+    '''Node of the memory grid.'''
+
     def __init__(self, name, size, used, avail):
         self.name = name
-        self.x, self.y = re.findall(r"x(\d+)-y(\d+)", self.name)[0]
+        match = re.findall(r"x(\d+)-y(\d+)", self.name)[0]
+        self.x, self.y = int(match[0]), int(match[1])
         self.size = size
         self.used = used
         self.avail = avail
@@ -28,42 +79,27 @@ class Node(object):
         return hash(self.name)
 
     def __str__(self):
-        return '({}, {}): {} {} {}'.format(self.x, self.y, self.size, self.used, self.avail)
+        return '({}) - {}/{}'.format(self.name, self.used, self.size)
 
     def __repr__(self):
-        return str(self)
+        return '{}/{}'.format(self.used, self.size)
+
+    def __eq__(self, other):
+        if isinstance(other, tuple):
+            return self.x == other[0] and self.y == other[1]
+        return self.x == other.x and self.y == other.y
 
 
 def parse_nodes():
     with open(FILENAME) as inputFile:
         inputFile.readline()
         inputFile.readline()
-        used_nodes = []
-        avail_nodes = []
+        nodes = []
         for line in inputFile:
             split = line.split()
-            used_nodes.append(Node(split[0], int(split[1][:-1]), int(split[2][:-1]), int(split[3][:-1])))
-            avail_nodes.append(Node(split[0], int(split[1][:-1]), int(split[2][:-1]), int(split[3][:-1])))
-        used_nodes.sort(key=lambda node: node.used, reverse=True)
-        avail_nodes.sort(key=lambda node: node.avail, reverse=True)
-
-        avail_visited = set()
-        pairs = []
-        used_pos = 0
-        avail_pos = 0
-        while used_pos < len(used_nodes):
-            used = used_nodes[used_pos]
-            if used.used == 0:
-                break
-            while avail_pos < len(avail_nodes) and avail_nodes[avail_pos].avail >= used.used:
-                avail_visited.add(avail_nodes[avail_pos])
-                avail_pos += 1
-            pairs.extend((used, avail) for avail in (avail_visited - {used}))
-            print(pairs)
-            print("==========================")
-            used_pos += 1
-
-        print(len(pairs))
+            nodes.append(Node(split[0], int(split[1][:-1]), int(split[2][:-1]), int(split[3][:-1])))
+        graph = Graph(nodes, start=(0, 0), goal=(36, 0))
+        print(graph)
 
 
 if __name__ == "__main__":
